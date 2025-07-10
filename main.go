4b09e30e8696a57e79f9e04376b1af25880c1175
rasagandy <raminsagandykov@gmail.com>
2025-07-10 15:08:21 +0500

    structure of the project defined, dal level interfaces defined

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-    
+	port := flag.String("port", "4000", "HTTP network address")
+	dir := flag.String("dir", "", "Path to the directory")
+	help := flag.Bool("help", false, "Print usage information")
+	flag.Parse()
+
+	if *help {
+		printUsage()
+		return
+	}
+
+	if *dir == "" {
+		log.Fatal("You must specify a directory with -dir")
+	}
+
+	mux := http.NewServeMux()
+
+	log.Printf("Listening on %s", *port)
+	err := http.ListenAndServe(*port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printUsage() {
+	fmt.Printf(`$ ./hot-coffee --help
+Coffee Shop Management System
+
+Usage:
+  hot-coffee [--port <N>] [--dir <S>] 
+  hot-coffee --help
+
+Options:
+  --help       Show this screen.
+  --port N     Port number.
+  --dir S      Path to the data directory.
+`)
 }
